ec31bb64024bfadb32774704efa9793504e6d935
coder-don <donald.a.stayner.ug@dartmouth.edu>
2015-05-26 22:18:48 -0400

    Copying tabs project to set up templates for each tab, and a controller js

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('iHamilton', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,66 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+// Set up state machine
+.config(function($stateProvider, $urlRouterProvider) {
+      // AngularUI Router
+      // Set up various states the app can be in
+      $stateProvider
+
+      // set up abstract state for the tabs directive
+      // so tabs are the highest level of state
+          .state('tab', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "templates/tabs.html"
+          })
+
+      // Each tab has its own nav history "stack"
+          .state('tab.numbers', {
+            url: '/numbers',
+            views: {
+              'tab-numbers': {
+                templateUrl: 'templates/tab-numbers.html',
+                controller: 'numbersCtrl'
+              }
+            }
+          })
+
+        // Each tab has its own nav history "stack"
+          .state('tab.dining', {
+            url: '/dining',
+            views: {
+              'tab-dining': {
+                templateUrl: 'templates/tab-dining.html',
+                controller: 'diningCtrl'
+              }
+            }
+          })
+
+        // Each tab has its own nav history "stack"
+          .state('tab.map', {
+            url: '/map',
+            views: {
+              'tab-map': {
+                templateUrl: 'templates/tab-map.html',
+                controller: 'mapCtrl'
+              }
+            }
+          })
+
+        // Each tab has its own nav history "stack"
+          .state('tab.spec', {
+            url: '/spec',
+            views: {
+              'tab-dining': {
+                templateUrl: 'templates/tab-spec.html',
+                controller: 'specCtrl'
+              }
+            }
+          });
+
+      // Default state (if none of other states are matched)
+      $urlRouterProvider.otherwise('/tab/numbers');
+
+
+    });
---
+++ www/js/controllers.js
@@ -0,0 +1,9 @@
+angular.module('iHamilton.controllers', [])
+
+.controller('numbersCtrl', function($scope) {})
+
+.controller('diningCtrl', function($scope) {})
+
+.controller('mapCtrl', function($scope) {})
+
+.controller('specCtrl', function($scope) {});
